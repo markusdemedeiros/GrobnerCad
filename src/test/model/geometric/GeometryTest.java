317858db49fe5f2f710ead66dac350e0f54007cd
markusdemedeiros <markusdemedeiros@outlook.com>
2020-02-22 13:10:58 -0800

    Implemented test suite for geometery

---
+++ src/test/model/geometric/GeometryTest.java
@@ -0,0 +1,23 @@
+package model.geometric;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GeometryTest {
+
+    @Test
+    public void testPoint() {
+        Point pt = new Point("A");
+        geometryImplementationTest(pt, "[PT : A]", "[PT : A]", "[PT : A]");
+    }
+
+    // Tests if geometry was implemented correctly
+    // Everything in the rest of the abstract class is just simple getters/setters
+    //      will be covered by other tests
+    private void geometryImplementationTest(Geometry testGeometery, String show, String showHuman, String showShort) {
+        assertEquals(show, testGeometery.show());
+        assertEquals(showHuman, testGeometery.showHuman());
+        assertEquals(showShort, testGeometery.showShort());
+    }
+}
